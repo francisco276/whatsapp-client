0afdcfcc5ec7650b0f2828f17449ce0509fded4e
DaniPoot <danielpoot.uc2@gmail.com>
2025-01-20 13:14:45 -0600

    ✨ feat(main.tsx): import '@vibe/core/tokens' to include Vibe tokens in the application for styling and theming

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import '@vibe/core/tokens'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
