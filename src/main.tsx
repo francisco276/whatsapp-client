54b22c87d3ffcf1dbc96a1d4cb27e3fa3991766a
DaniPoot <danielpoot.uc2@gmail.com>
2025-05-20 15:48:13 -0600

    🔧 fix(main.tsx): update import path for vibe core tokens CSS file to fix styling issue

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import '@vibe/core/tokens'
+import "../node_modules/@vibe/core/dist/tokens/tokens.css";
 import Providers from './components/providers.tsx'
 
 createRoot(document.getElementById('root')!).render(
