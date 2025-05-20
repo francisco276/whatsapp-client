65da976ce22b5a98ed8ab15719e7e30e80773a1c
DaniPoot <danielpoot.uc2@gmail.com>
2025-05-20 14:45:02 -0600

    ✨ feat(useContext.ts): add useContext hook to manage context state in React components

---
+++ src/hooks/useContext.ts
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react"
+import { MondayApi } from "../lib/monday/api"
+import type { AppContext } from "../types/monday"
+
+type ContextProps = {
+  monday: MondayApi
+}
+
+export const useContext = ({ monday }: ContextProps) => {
+  const [contextState, setContextState] = useState<AppContext>({
+    workspaceId: '',
+    itemId: '',
+    userId: '',
+    boardId: '',
+    idleItemId: '',
+    userPunchesBoardID: '',
+    version: '',
+    theme: ''
+  })
+
+  const getContext = async () => {
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    const context: any = await monday.getContext()
+
+    return context
+  }
+
+
+  const loadContext = async () => {
+    const context = await getContext()
+
+    setContextState((state) => {
+      return {
+        ...state,
+        workspaceId: context?.data?.workspaceId,
+        itemId: context?.data?.itemId,
+        boardId: context?.data?.boardId,
+        userId: context?.data?.user.id,
+        theme: context?.data?.theme,
+        version: `${context?.data?.appVersion.versionData.major}.${context?.data?.appVersion.versionData.minor}`,
+      }
+    })
+  }
+
+  useEffect(() => {
+    loadContext()
+  }, [])
+
+  return {
+    context: contextState
+  }
+}
