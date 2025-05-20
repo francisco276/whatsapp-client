0160e88bcf2ae27636fcd0ffcf11023e0f0a162e
DaniPoot <danielpoot.uc2@gmail.com>
2025-05-20 14:45:38 -0600

    ✨ feat(types): add AppContext type to define the structure of context data for Monday.com integration

---
+++ src/types/monday.ts
@@ -0,0 +1,10 @@
+export type AppContext = {
+  workspaceId: string
+  itemId: string
+  userId: string
+  boardId: string
+  idleItemId: string
+  userPunchesBoardID: string
+  version: string
+  theme: string
+}
