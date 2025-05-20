a10c5a1069559045d377404c5dffc6081318e98d
DaniPoot <danielpoot.uc2@gmail.com>
2025-05-20 14:50:33 -0600

    ✨ feat(socket.ts): add SocketClient class to handle socket connections and events in the application

---
+++ src/lib/socket.ts
@@ -0,0 +1,32 @@
+import { io } from 'socket.io-client'
+import type { Socket } from 'socket.io-client'
+import type { SocketSuccessResponse, SocketErrorResponse } from '../types/socket'
+
+const URL: string | undefined = import.meta.env.VITE_SOCKET_API ?? undefined
+const API_KEY: string | undefined = import.meta.env.VITE_API_KEY ?? undefined
+
+export class SocketClient {
+  private readonly socket: Socket
+
+  constructor({ workspaceId, sessionId }: { workspaceId: string, sessionId: string }) {
+    this.socket = io(URL, {
+      path: '/socket.io',
+      autoConnect: true,
+      auth: {
+        token: API_KEY
+      },
+      query: {
+        workspaceId,
+        sessionId
+      },
+    })
+  }
+
+  on(event: string, callback: (data: SocketSuccessResponse | SocketErrorResponse) => void | Promise<void>) {
+    return this.socket.on(event, callback)
+  }
+
+  disconnect() {
+    this.socket.disconnect()
+  }
+}
