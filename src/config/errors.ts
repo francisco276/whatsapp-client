5af3a7e9a2e010e3550a9e4063b0511a760f607f
DaniPoot <danielpoot.uc2@gmail.com>
2025-05-20 14:46:28 -0600

    ✨ feat(errors.ts): add error messages for server errors and default errors to improve user experience

---
+++ src/config/errors.ts
@@ -0,0 +1,4 @@
+export const ERROR_SERVER_ERROR =
+  'An error occurred on the server. If this issue persists, please reach out to technical support for help.'
+export const DEFAULT_ERROR =
+  'An unexpected error occurred. Please contact support for help.'
