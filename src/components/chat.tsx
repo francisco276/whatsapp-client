276bd304369264da0584a1698647d4f79055feb4
DaniPoot <danielpoot.uc2@gmail.com>
2025-05-20 15:05:08 -0600

    🚀 feat(chat.tsx): add Chat component to handle messaging functionality in the app

---
+++ src/components/chat.tsx
@@ -0,0 +1,83 @@
+import { Flex } from '@vibe/core'
+import { useContext, useCallback } from "react"
+import { useQuery, useInfiniteQuery } from "@tanstack/react-query"
+import { SessionContext } from './providers/session/session-context'
+import { ChatContext } from "./providers/chat/chat-context"
+import { getMessages } from "../lib/services/messages"
+import { MessagesList } from "./messages/messages-list"
+import { MessageInput } from "./messages/message-input"
+import { getContact } from "../lib/services/contacts"
+import { Error } from '../components/error'
+import { ERROR_SERVER_ERROR } from '../config/errors'
+import { mapMessageToElement, groupMessagesByDate } from '../utils/message'
+
+type ChatProps = {
+  workspaceId: string
+}
+
+export const Chat = ({ workspaceId }: ChatProps) => {
+  const { session } = useContext(SessionContext)
+  const { chat } = useContext(ChatContext)
+
+  const {
+    data: messagesData,
+    isError,
+    isFetching,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ['messages', session, chat, workspaceId],
+    queryFn: ({ pageParam }: { pageParam?: number | string }) => getMessages({
+      workspaceId,
+      sessionId: session,
+      chatId: chat,
+      offset: pageParam
+    }),
+    enabled: !!(session && chat),
+    initialPageParam: undefined,
+    getNextPageParam: (lastPage) => lastPage.offset,
+  })
+
+  const { data: contact } = useQuery({
+    queryKey: ['getContact', chat],
+    queryFn: () => getContact({ id: chat, workspaceId, sessionId: session }),
+    enabled: !!chat
+  })
+
+  const allMessages = groupMessagesByDate((messagesData?.pages.flatMap(page => page.data) ?? [])
+    .map(message => mapMessageToElement(message)).reverse())
+
+  const handleScroll = useCallback(async () => {
+    if (hasNextPage && !isFetchingNextPage) {
+      await fetchNextPage()
+    }
+  }, [hasNextPage, isFetchingNextPage, fetchNextPage])
+
+  return (
+    <div className="flex-1 flex flex-col">
+      <div className="bg-white p-4 shadow">
+        <h1 className="font-bold">
+          {contact?.name}
+        </h1>
+      </div>
+      {isError && <Flex align='center' justify='center' className='m-auto w-max-[80%]' ><Error errorMessage={ERROR_SERVER_ERROR} /></Flex>}
+      {
+        !isError && (
+          <div className="p-4 flex-1 flex">
+            {
+              <MessagesList
+                messages={allMessages}
+                isLoading={isFetching || isFetchingNextPage}
+                onScroll={handleScroll}
+              />
+            }
+          </div>
+        )
+      }
+      <div className="p-4 border-t">
+        <MessageInput />
+      </div>
+    </div>
+  )
+}
