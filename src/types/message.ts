bf4c2d1f36ed1a96f35ad689d24fbc8dae95b2c9
DaniPoot <danielpoot.uc2@gmail.com>
2025-05-20 14:59:32 -0600

    ✨ feat(message.ts): add new types for messages to improve code readability and maintainability

---
+++ src/types/message.ts
@@ -0,0 +1,98 @@
+type MessageKey = {
+  id: string
+  fromMe: boolean
+  remoteJid: string
+  participant: string
+}
+
+export type WMessage = {
+  conversation?: string
+  extendedTextMessage?: {
+    text: string,
+    contextInfo: {
+      isForwarded: boolean
+      forwardingScore: number
+    }
+  },
+  stickerMessage?: {
+    url: string,
+    contextInfo: {
+      isForwarded: boolean
+      forwardingScore: number
+    }
+  },
+  imageMessage?: {
+    url: string
+    caption: string,
+    contextInfo: {
+      isForwarded: boolean
+      forwardingScore: number
+    }
+  },
+  videoMessage?: {
+    url: string
+    gifPlayback: boolean
+    caption: string,
+    contextInfo: {
+      isForwarded: boolean
+      forwardingScore: number
+    }
+  },
+  documentMessage: {
+    url: string
+    mimetype: string
+    caption?: string
+    title: string,
+    contextInfo: {
+      isForwarded: boolean
+      forwardingScore: number
+    }
+  }
+}
+
+export type Message = {
+  pkId: number
+  sessionId: string
+  workspaceId: string
+  remoteJid: string
+  id: string
+  broadcast: boolean
+  key: MessageKey
+  message: WMessage
+  messageTimestamp: number
+  pushName: string
+  status: number
+}
+
+export type MessageList = {
+  data: Message[]
+  cursor: number | string
+  offset: number | string
+}
+
+export type MessageContent = {
+  text: string
+  url?: string
+  isImage: boolean
+  isVideo: boolean
+  isDocument: boolean
+  isSticker: boolean
+  type?: string
+  documentTitle?: string
+  isForwarded: boolean
+  isGift?: boolean
+}
+
+export type MessageItem = {
+  id: string
+  isAGroup: boolean,
+  isMyMessage: boolean
+  timestamp: Date,
+  message: MessageContent,
+  originalMessage?: Message,
+  participant: string,
+  workspaceId: string,
+  sessionId: string
+  pushName: string
+  isDateSeprator?: boolean
+}
