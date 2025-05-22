ee9ec1f184029126174b6d3094efc10188004a47
DaniPoot <danielpoot.uc2@gmail.com>
2025-05-22 16:23:41 -0600

    🚀 feat(home-page.tsx): add Loader component and FormWorkspace component to improve user experience 🔧 fix(home-page.tsx): update layout structure for better readability and maintainability

--- src/page/home-page.tsx
+++ src/page/home-page.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@vibe/core'
+import { Box, Flex, Loader } from '@vibe/core'
 import { SessionSidebar } from '../components/sessions-sidebar'
 import { ChatsSidebar } from '../components/chats-sidebar'
 import { Chat } from '../components/chat'
@@ -10,11 +10,21 @@ import { useContext } from '../hooks/useContext.ts'
 import { MondayApi } from '../lib/monday/api'
 import { Error } from '../components/error'
 import { ERROR_SERVER_ERROR } from '../config/errors'
+import { getWorkspace } from '../lib/services/workspaces.ts'
+import { FormWorkspace } from '../components/form-workspace.tsx'
 
 export default function HomePage() {
   const monday = new MondayApi()
   const { context } = useContext({ monday })
   const { workspaceId } = context
+
+  const {
+    isError: isErrorWorkspace,
+    isLoading: isLoadingWorkspace
+  } = useQuery({
+    queryKey: ['getWorkspace', workspaceId],
+    queryFn: () => getWorkspace({ workspaceId }),
+  })
 
   const {
     data,
@@ -27,29 +37,39 @@ export default function HomePage() {
   })
 
   return (
-    <SessionProvider>
-      <Box>
-        <Flex align='stretch' className='min-h-screen'>
-          <SessionSidebar
-            workspaceId={workspaceId}
-            sessions={data?.sessions || []}
-            isLoading={isLoading}
-            error={isError}
-          />
-          {isError && <Flex align='center' justify='center' className='mx-auto' ><Error errorMessage={ERROR_SERVER_ERROR} /></Flex>}
-          {
-            (!isError && !isLoading) &&
-            (
+    <>
+
+      <Flex className='w-screen h-screen' justify='center' align='center' >
+        {isLoadingWorkspace && <Loader size="large" />}
+        {isErrorWorkspace && <FormWorkspace workspaceId={workspaceId} />}
+      </Flex>
+      {!isErrorWorkspace && (
+        <SessionProvider>
+          <Box>
+            <Flex align='stretch' className='min-h-screen'>
+              <SessionSidebar
+                workspaceId={workspaceId}
+                sessions={data?.sessions || []}
+                isLoading={isLoading}
+                error={isError}
+              />
+              {isError && <Flex align='center' justify='center' className='mx-auto' ><Error errorMessage={ERROR_SERVER_ERROR} /></Flex>}
+
+              (!isError && !isLoading) &&
+              (
               <ChatProvider>
                 <>
                   <ChatsSidebar workspaceId={workspaceId} />
                   <Chat workspaceId={workspaceId} />
                 </>
               </ChatProvider>
-            )
-          }
-        </Flex>
-      </Box>
-    </SessionProvider>
+              )
+            </Flex>
+          </Box>
+        </SessionProvider>
+      )
+      }
+    </>
+
   )
 }
