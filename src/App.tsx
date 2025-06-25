accc8a34ac6f60e2bd1044de493096a09b20c175
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 15:14:36 -0600

    ✨ feat(App.tsx): add SingleChatPage component to support a new route '/single' for single chat functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'wouter'
 import HomePage from './page/home-page'
 import ConfigurationPage from './page/configuration-page'
+import SingleChatPage from './page/single-chat-page'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Switch>
         <Route path="/" component={HomePage} />
         <Route path="/config" component={ConfigurationPage} />
+        <Route path="/single" component={SingleChatPage} />
       </Switch>
     </>
   )
