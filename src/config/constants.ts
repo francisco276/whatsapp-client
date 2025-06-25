6a2a3eaa52d3cb53715c5c591c7dc11673166721
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 13:58:31 -0600

    ✨ feat(constants.ts): add API_TIMEOUT and AUTHORIZATION_TYPES const values

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,8 @@
 export const MONDAY_API_VERSION = '2025-04'
+export const API_TIMEOUT = 60000
+
+export const AUTHORIZATION_TYPES = {
+  'All': 'Todos',
+  'Authorized': 'Autorizados',
+  'Unauthorized': 'No autorizados'
+}
