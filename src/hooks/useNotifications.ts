307618b0e67bceeae41717678f6b95728e4fb55a
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 15:45:08 -0600

    ✨ feat(useNotifications.ts): add useNotifications hook to handle sending notifications to authorized users based on workspaceId and context data

---
+++ src/hooks/useNotifications.ts
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query'
+import { MondayApi } from '@/lib/monday/api'
+import { getAuthorizationUsers } from '@/lib/services/authorization'
+import { useWorkspaceId } from './useWorkspaceId'
+import { useContext } from './useContext'
+
+export const useNotifications = () => {
+  const monday = new MondayApi()
+  const { context } = useContext({ monday })
+  const { itemId, boardId } = context
+  const workspaceId = useWorkspaceId()
+
+  const { data: authorizedUsersResponse } = useQuery({
+    queryKey: ['authorizeUsers'],
+    queryFn: () => getAuthorizationUsers({ workspaceId })
+  })
+
+  async function sendNotifications() {
+    const notifications = authorizedUsersResponse?.authorizations
+      .map(async (user) => await monday.mutation.createNotification(user.userId, itemId || boardId, "Un mensaje nuevo se ha enviado"))
+
+    if (Array.isArray(notifications) && notifications.length > 0) {
+      await Promise.all(notifications)
+    }
+  }
+
+  return {
+    sendNotifications
+  }
+}
