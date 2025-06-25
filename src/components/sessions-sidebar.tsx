10567d3c2918a44c58cbce8dfc17ae7804b81538
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 15:38:24 -0600

    🚀 feat(sessions-sidebar.tsx): update SessionSidebar component to use MondayApi for modal functionality and add Settings icon for configuration modal opening

--- src/components/sessions-sidebar.tsx
+++ src/components/sessions-sidebar.tsx
@@ -4,10 +4,10 @@ import { SessionContext } from './providers/session/session-context'
 import { AddSession } from './add-session'
 import { SessionButton } from './session-button'
 
-import { NavigationChevronLeft, NavigationChevronRight } from '@vibe/icons'
+import { NavigationChevronLeft, NavigationChevronRight, Settings } from '@vibe/icons'
 import { IconButton } from '@vibe/core'
 import { SideBarList } from './skeletons/sidebar-list'
-
+import { MondayApi } from '../lib/monday/api'
 
 type Session = {
   id: string
@@ -19,27 +19,31 @@ type SessionSidebarProps = {
   error?: boolean
 }
 
-const SessionSidebar = ({ workspaceId, sessions = [], isLoading = false, error }: SessionSidebarProps ) => {
+const SessionSidebar = ({ sessions = [], isLoading = false, error }: SessionSidebarProps) => {
+  const monday = new MondayApi()
   const { session: currentSession, setSession } = useContext(SessionContext)
   const [sessionsSidebarOpen, setSessionsSidebarOpen] = useState(true)
 
-  return  <div className={`transition-all duration-300 flex flex-col ${
-    sessionsSidebarOpen ? 'w-64' : 'w-16'
-  }`}>
-    <div className="p-4 flex justify-between items-center">
+  function handleOpenModal() {
+    monday.execute("openAppFeatureModal", { urlPath: '/config', width: 400 })
+  }
+
+  return <div className={`transition-all duration-300 flex flex-col ${sessionsSidebarOpen ? 'w-64' : 'w-16'
+    }`}>
+    <div className="p-4 flex items-center">
       <h2 className={`font-bold ${!sessionsSidebarOpen && 'hidden'}`}>Cuentas</h2>
-      { sessionsSidebarOpen }
+      {sessionsSidebarOpen && <IconButton className="ml-auto" size='small' icon={Settings} onClick={handleOpenModal} />}
       <IconButton
         onClick={() => setSessionsSidebarOpen(!sessionsSidebarOpen)}
         size='small'
-        ariaLabel={sessionsSidebarOpen ? 'Cerrar' : 'Abrir' }
-        icon={sessionsSidebarOpen ? NavigationChevronLeft : NavigationChevronRight }
+        ariaLabel={sessionsSidebarOpen ? 'Cerrar' : 'Abrir'}
+        icon={sessionsSidebarOpen ? NavigationChevronLeft : NavigationChevronRight}
       />
     </div>
-    
+
     <div className="p-2 flex-1 flex flex-col">
-      <AddSession disabled={isLoading || error} isToggle={sessionsSidebarOpen} workspaceId={workspaceId} />
-      { isLoading && <SideBarList /> }
+      <AddSession disabled={isLoading || error} isToggle={sessionsSidebarOpen} />
+      {isLoading && <SideBarList />}
       {!isLoading && sessions.map(session => (
         <SessionButton
           sessionId={session.id}
