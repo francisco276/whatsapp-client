9bc3012761a8c8efb6fb2064e7bdaccaf795a05a
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 14:12:14 -0600

    ✨ feat(App.tsx): add ConfigurationPage component to handle /config route for additional functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Switch } from 'wouter'
 import HomePage from './page/home-page'
+import ConfigurationPage from './page/configuration-page'
 
 function App() {
   return (
     <>
       <Switch>
         <Route path="/" component={HomePage} />
+        <Route path="/config" component={ConfigurationPage} />
       </Switch>
     </>
   )
