def3b5794486e89148142d519d49656237e0d7ea
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 14:01:52 -0600

    ✨ feat(preferences.ts): add services for updating and getting user preferences by user and workspace ID

---
+++ src/lib/services/preferences.ts
@@ -0,0 +1,35 @@
+import { api } from "../axios"
+import type { UserPreferencesConfig } from '@/types'
+
+const ROUTE = '/preferences/'
+
+
+type GetConfigurationParasm = { workspaceId: string, userId: string }
+type UpdateConfigurationParams = { config: UserPreferencesConfig } & GetConfigurationParasm
+
+export const updateConfigurationByUser = async ({ workspaceId, userId, config }: UpdateConfigurationParams) => {
+  try {
+    const response = await api.post(`${ROUTE}update`, {
+      workspaceId,
+      userId,
+      config
+    })
+
+    return response.data
+  } catch (error) {
+    throw new Error('Error adding a autorization user')
+  }
+}
+
+export const getConfigurationByUser = async ({ workspaceId, userId }: GetConfigurationParasm) => {
+  try {
+    const response = await api.post<UserPreferencesConfig>(`${ROUTE}`, {
+      workspaceId,
+      userId
+    })
+
+    return response.data
+  } catch (error) {
+    throw new Error('Error adding a autorization user')
+  }
+}
