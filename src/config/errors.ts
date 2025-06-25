e73370a587a47b61bbb864a62075e4d2721ab252
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 15:36:25 -0600

    🚀 feat(errors.ts): add new error constants for invalid phone number and incorrect phone column configuration

--- src/config/errors.ts
+++ src/config/errors.ts
@@ -2,3 +2,7 @@ export const ERROR_SERVER_ERROR =
   'An error occurred on the server. If this issue persists, please reach out to technical support for help.'
 export const DEFAULT_ERROR =
   'An unexpected error occurred. Please contact support for help.'
+
+export const ERROT_ITEM_NOT_FOUNT = 'An error ocurred. The item was not found'
+export const ERROR_PHONE_NUMBER_INVALID = 'An error ocurred. The phone number is invalid. Please check the phone number format and try again.'
+export const ERROR_PHONE_COLUMN_CONFIGURATION = 'An error ocurred. The phone column is not configured correctly. Please check the column settings and try again'
