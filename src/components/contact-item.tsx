b715f7c8e7764354e83fdd6bae79341098833cc6
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 15:42:30 -0600

    🚀 feat(contact-item.tsx): introduce useMutation hook for updating read chat status

--- src/components/contact-item.tsx
+++ src/components/contact-item.tsx
@@ -1,11 +1,14 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation } from '@tanstack/react-query'
 import { SessionContext } from './providers/session/session-context'
 import { ChatContext } from './providers/chat/chat-context'
 import { getContact } from '../lib/services/contacts'
 
-import { Button, Avatar, Skeleton } from '@vibe/core'
+import { Button, Avatar, Skeleton, Badge } from '@vibe/core'
 import { Comment } from '@vibe/icons'
 import { useMemo, useContext } from 'react'
+import { unreadChatStore } from '@/stores/unReadChatStore'
+import { updateReadChat } from '@/lib/services/chats'
+import { jidToFormatedPhone } from '@/utils/whatsapp'
 
 type ContactItemProps = {
   contactId: string
@@ -14,6 +17,8 @@ type ContactItemProps = {
 }
 
 export const ContactItem = ({ contactId, isToggle, workspaceId }: ContactItemProps) => {
+  const unread = unreadChatStore((state) => state.contacts[contactId])
+  const resetChat = unreadChatStore((state) => state.resetUnreadChat)
   const { session: sessionId } = useContext(SessionContext)
   const { chat, setChat } = useContext(ChatContext)
   const { data: contact, isLoading } = useQuery({
@@ -23,27 +28,48 @@ export const ContactItem = ({ contactId, isToggle, workspaceId }: ContactItemPro
     staleTime: 1440 * 60 * 1000
   })
 
+  const { mutate } = useMutation({
+    mutationFn: updateReadChat
+  })
+
   const isSelected = useMemo(() => chat === contactId, [chat, contactId])
   const Icon = useMemo(() => {
     if (isLoading) {
-      return () => (<Skeleton type="circle" size='small' className={!isToggle ? '!size-4 !px-0' : ''} />)
+      return () => (<Skeleton type="circle" size="p" />)
     }
-    return contact?.image ? () => (<div className='min-w-[28px]'><Avatar className={!isToggle ? '!size-4 !px-0' : ''} size="small" type="img" src={contact.image} withoutBorder={true} /></div>) : Comment
+    return () => (
+      <div >
+        {contact?.image ? <Avatar size={isToggle ? 'medium' : 'small'} type="img" src={contact.image} withoutBorder={true} /> : <Avatar size={isToggle ? 'medium' : 'small'} type="icon" icon={Comment} withoutBorder={true} />}
+      </div>
+    )
   }, [isLoading, contact, isToggle])
 
-  return (
-    <Button
-      className={`!w-full rounded !flex !justify-start !overflow-clip ${isSelected ? '!bg-blue-100' : '!hover:bg-gray-200'
-        }`}
-      kind='secondary'
-      leftIcon={Icon}
-      onClick={() => {
-        setChat(contactId)
-      }}
-    >
-      {
-        isToggle && <span className='text-left pl-2'>{contact?.name || contact?.verifiedName || contact?.notify}</span>
+
+  const button = <Button
+    className={`w-full ${isToggle ? '!justify-start' : '!pr-[8px]'}  ${isSelected ? '!bg-blue-100' : '!hover:bg-gray-200'} `}
+    kind='secondary'
+    leftIcon={Icon}
+    onClick={() => {
+      setChat(contactId)
+      if (unread) {
+        resetChat(contactId)
+        mutate({ id: contactId, workspaceId, sessionId })
       }
-    </Button>
+    }}
+  >
+    <div className='ml-2 w-[50%] flex-auto'>
+      {
+        isToggle && <p className='truncate text-left'>{contact?.name || contact?.verifiedName || contact?.notify || jidToFormatedPhone(contact?.id)}</p>
+      }
+    </div>
+
+  </Button>
+
+  return (
+    <div className='ml-1'>
+      {
+        unread ? <Badge color='notification' anchor='top-start'> {button} </Badge> : button
+      }
+    </div>
   )
 }
