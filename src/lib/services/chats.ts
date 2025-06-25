0fb09772602fed2c06da0fd75e04276c0b4fefcb
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 15:25:13 -0600

    🚀 feat(chats.ts): add support for updating read chat status by sending a POST request to the server with chat id

--- src/lib/services/chats.ts
+++ src/lib/services/chats.ts
@@ -9,11 +9,27 @@ export type Chat = {
   description: string
   image?: string
   lastMessageRecvTimestamp: string
+  unreadCount: number
 }
 
 export const getChats = async ({ workspaceId, sessionId }: { workspaceId: string, sessionId: string }) => {
   try {
-    const response = await api.get(`${ROUTE}/${workspaceId}/${sessionId}`, { timeout: 0 })
+    const response = await api.get(`/${workspaceId}/${sessionId}${ROUTE}`, { timeout: 0 })
+
+    return response.data as { chats: Chat[] }
+  } catch (error) {
+    throw new Error('Error on fetch chats')
+  }
+}
+
+export const updateReadChat = async ({ workspaceId, sessionId, id }: { workspaceId: string, sessionId: string, id: string }) => {
+  try {
+    const response = await api.post(
+      `/${workspaceId}/${sessionId}${ROUTE}/read`,
+      {
+        id
+      },
+    )
 
     return response.data as { chats: Chat[] }
   } catch (error) {
