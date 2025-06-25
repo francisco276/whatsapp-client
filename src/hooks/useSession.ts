1f2964dcbb15f5560fbe3e3e6145c2d925855755
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 14:08:58 -0600

    🚀 feat(useSession.ts): add custom hooks for managing sessions, session information, and deleting sessions in the application

---
+++ src/hooks/useSession.ts
@@ -0,0 +1,41 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getSessions, deleteSession } from '@/lib/services/sessions'
+import { getProfile } from '@/lib/services/profile'
+
+type UseSessionsProps = {
+  workspaceId: string
+}
+
+export const useSessions = ({ workspaceId }: UseSessionsProps) => {
+  return useQuery({
+    queryKey: ['getSessions', workspaceId],
+    queryFn: () => getSessions({ workspaceId }),
+    enabled: !!workspaceId,
+  })
+}
+
+type UseSessionInformationProps = {
+  workspaceId: string
+  sessionId: string
+}
+
+export const useSessionInformation = ({ workspaceId, sessionId }: UseSessionInformationProps) => {
+  return useQuery({
+    queryKey: [sessionId],
+    queryFn: () => getProfile({ workspaceId, sessionId }),
+    refetchOnWindowFocus: false,
+    staleTime: 1440 * 60 * 1000,
+    enabled: !!workspaceId && !!sessionId
+  })
+}
+
+export const useDeleteSession = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: deleteSession,
+    onSuccess: (_, { workspaceId }) => {
+      queryClient.invalidateQueries({ queryKey: ['getSessions', workspaceId] })
+    }
+  })
+}
