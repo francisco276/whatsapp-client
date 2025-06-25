e3430aca6951dc47311e7867f91923edb4b4424b
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 15:13:07 -0600

    🚀 feat(lib): add new function getSingleChatInformation to retrieve chat information for a single item in Monday API

---
+++ src/lib/index.ts
@@ -0,0 +1,38 @@
+import { MondayApi } from '@/lib/monday/api'
+import { formatPhoneToWhatsAppJID } from "@/utils/whatsapp"
+import { isValidContact } from "@/lib/services/contacts"
+import { getColumnById } from '@/utils/utils'
+import { PublicError } from '@/errors/PublicError'
+import { ERROR_PHONE_NUMBER_INVALID, ERROT_ITEM_NOT_FOUNT } from '@/config/errors'
+
+
+export async function getSingleChatInformation({
+  monday,
+  workspaceId,
+  sessionId,
+  phoneColumnId,
+  itemId
+}: { monday: MondayApi, workspaceId: string, sessionId: string, phoneColumnId: string, itemId: string }) {
+  try {
+    const { data } = await monday.query.getPhoneColumnsByIdsForItem(itemId)
+    const item = data.items[0]
+
+    const columnValues = item['column_values']
+    if (!item || !columnValues) throw new PublicError(ERROT_ITEM_NOT_FOUNT)
+
+    const phoneColumn = getColumnById({ columnValues, columnId: phoneColumnId })
+
+    if (!phoneColumn) throw new PublicError(ERROR_PHONE_NUMBER_INVALID)
+
+    const id = formatPhoneToWhatsAppJID(phoneColumn.phone!, phoneColumn.country_short_name!)
+
+    const isValid = await isValidContact({ workspaceId, sessionId, id })
+
+    return {
+      isValid,
+      chatId: id
+    }
+  } catch (error) {
+    console.log({ error })
+  }
+}
