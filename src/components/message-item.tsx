6a9e69758df0df1d4b484d7482ba0179d3474fdd
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 15:42:55 -0600

    🔧 fix(message-item.tsx): add Loader component to show loading state when media is being fetched

--- src/components/message-item.tsx
+++ src/components/message-item.tsx
@@ -4,9 +4,10 @@ import type { MessageItem } from "../types/message"
 import { downloadMedia } from "../lib/services/messages"
 import { MessageImageComponent } from './messages/formats/image'
 import { MessageDocumentComponent } from './messages/formats/document'
-import { Icon } from '@vibe/core'
+import { Icon, Loader } from '@vibe/core'
 import { Forward } from '@vibe/icons'
 import { MessageVideoComponent } from './messages/formats/video'
+import { getTime } from '@/utils/time'
 
 export const MessageItemComponent = ({ message }: { message: MessageItem }) => {
   const {
@@ -38,7 +39,7 @@ export const MessageItemComponent = ({ message }: { message: MessageItem }) => {
     staleTime: 1440 * 60 * 1000
   })
 
-  const { data: mediaUrl } = useQuery({
+  const { data: mediaUrl, isLoading: isLoadingMedia } = useQuery({
     queryKey: ['getMedia', message],
     queryFn: () => downloadMedia({ workspaceId, sessionId, message: originalMessage }),
     enabled: (isImage || isVideo || isSticker),
@@ -49,32 +50,42 @@ export const MessageItemComponent = ({ message }: { message: MessageItem }) => {
 
   return (
     <div
-      className={`flex mb-2.5 
-        ${isDateSeprator && 'justify-center'}
-        ${(isMyMessage && !isDateSeprator) && 'justify-end'}
-          ${(!isMyMessage && !isDateSeprator) && 'justify-start'}
+      className={`flex mb-2.5 px-4
+        ${isDateSeprator ? 'justify-center' : ''}
+        ${(isMyMessage && !isDateSeprator) ? 'justify-end' : ''}
+          ${(!isMyMessage && !isDateSeprator) ? 'justify-start' : ''}
       `}
     >
       <div
-        className={`max-w-xs md:max-w-md rounded-lg px-4 py-2
-          ${isDateSeprator && 'bg-stone-950 text-white'}
-          ${(isMyMessage && !isDateSeprator) && '!bg-blue-500 !text-white rounded-br-none'}
-          ${(!isMyMessage && !isDateSeprator) && '!bg-gray-200 !rounded-bl-none'}
+        className={`max-w-xs md:max-w-md rounded-lg text-wrap text-ellipsis whitespace-pre-line
+          ${(isMyMessage && !isDateSeprator) && '!bg-blue-500 !text-white rounded-br-none px-4 py-2'}
+          ${(!isMyMessage && !isDateSeprator) && '!bg-gray-200 !rounded-bl-none px-4 py-2'}
         `}
       >
         {!isMyMessage && isAGroup && (
-          <div className="font-semibold text-sm">{contact?.name || message.pushName}</div>
+          <div className="text-sm">{contact?.name || message.pushName}</div>
         )}
 
         {isForwarded && <p className="text-gray-500 flex gap-2 items-center"><Icon iconType="svg" icon={Forward} iconLabel="forwarded" iconSize={16} />Forwarded</p>}
 
+        {isLoadingMedia && <Loader size="small" />}
         {((isImage || isSticker) && mediaUrl) && <MessageImageComponent url={mediaUrl} />}
         {(isDocument && documentTitle) && <MessageDocumentComponent name={documentTitle} />}
         {(isVideo && mediaUrl) && <MessageVideoComponent url={mediaUrl} isGift={isGift!} />}
-        <p>{messageString}</p>
+        {
+          isDateSeprator && (
+            <div className="flex justify-center">
+              <div className="bg-gray-100 text-gray-600 text-sm px-4 py-2 rounded-full font-medium">
+                {messageString}
+              </div>
+            </div>
+          )
+        }
+        {!isDateSeprator && messageString}
 
-        <div className={`text-xs mt-1 ${isMyMessage ? '!text-blue-100' : '!text-gray-500'}`}>
-          {!isDateSeprator && timestamp.toString()}
+
+        <div className={`text-xs mt-1 text-right ${isMyMessage ? '!text-blue-100' : '!text-gray-500'}`}>
+          {!isDateSeprator && getTime({ date: timestamp })}
         </div>
       </div>
     </div>
