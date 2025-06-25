7a7ba65af631517612da1656f95b763eb6c2cae0
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 15:34:07 -0600

    🎨 style(document.tsx): refactor MessageDocumentComponent to use Flex component for better layout

--- src/components/messages/formats/document.tsx
+++ src/components/messages/formats/document.tsx
@@ -1,9 +1,17 @@
+import { Flex, Icon } from '@vibe/core'
+import { Doc } from '@vibe/icons'
+
 export const MessageDocumentComponent = ({
   name
 }: { name: string, type?: string }) => {
   return (
-    <div>
-      {name}
-    </div>
+    <Flex
+      align='center'
+      gap={10}
+      className='bg-blue-400 rounded-sm p-2 bg-blend-soft-light'
+    >
+      <Icon icon={Doc} />
+      <p className='text-sm'>{name}</p>
+    </Flex>
   )
 }
