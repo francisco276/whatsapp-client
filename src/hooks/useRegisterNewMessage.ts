c179e49267bb8457c79bb2fe520f9c20a61f4d59
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 15:39:36 -0600

    🚀 feat(useRegisterNewMessage.ts): add custom hook useRegisterNewMessage to handle new messages and update unread chat count in the chat application.

---
+++ src/hooks/useRegisterNewMessage.ts
@@ -0,0 +1,34 @@
+import { useContext, useEffect, useMemo } from 'react'
+import { SessionContext } from '@/components/providers/session/session-context'
+import { ChatContext } from '@/components/providers/chat/chat-context'
+import { SocketClient } from '@/lib/socket'
+import { handlerNotifyMessage } from '@/lib/socket-handlers/messages'
+import { unreadChatStore } from '@/stores/unReadChatStore'
+import { Chat } from '@/lib/services/chats'
+
+
+export const useRegisterNewMessage = ({ workspaceId, chats }: { workspaceId: string, chats?: Chat[] }) => {
+  const { session } = useContext(SessionContext)
+  const { chat } = useContext(ChatContext)
+  const incrementUnreadChat = unreadChatStore((state) => state.incrementUnreadChat)
+
+  const socket = useMemo(() => new SocketClient({ workspaceId, sessionId: session }), [workspaceId, session])
+
+  useEffect(() => {
+    handlerNotifyMessage(socket, ({ id }) => {
+      if (chat !== id) incrementUnreadChat(id)
+    })
+
+    return (() => {
+      socket.disconnect()
+    })
+  }, [socket])
+
+  useEffect(() => {
+    chats?.forEach((chat) => {
+      if (chat.unreadCount) {
+        incrementUnreadChat(chat.id)
+      }
+    })
+  }, [chats])
+}
