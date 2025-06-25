a86ecd0d2eec06f31a2f67802a43bd117d8b1f6c
DaniPoot <danielpoot.uc2@gmail.com>
2025-06-25 13:57:09 -0600

    🚀 feat(types): introduce new types MondayError, MondayResponse, AppSettings, ColumnValue, MondayListenResponse, SingleSettings, and User for better type safety and clarity

--- src/types/monday.ts
+++ src/types/monday.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { CountryCode } from "libphonenumber-js"
+
 export type AppContext = {
   workspaceId: string
   itemId: string
@@ -7,4 +11,55 @@ export type AppContext = {
   userPunchesBoardID: string
   version: string
   theme: string
+}
+
+export type MondayError = {
+  message: string
+  locations: {
+    line: number
+    column: number
+  }[]
+  path: string[]
+  extensions: {
+    code: string
+    error_data: object
+    status_code: number
+  }
+}
+
+export type MondayResponse<T> = {
+  data: {
+    [key: string]: any
+  } | T
+  errors: MondayError[]
+  account_id: string
+}
+
+export type AppSettings = {
+  phoneColumnId: string
+}
+
+export type ColumnValue = {
+  id: string
+  value: string
+  country_short_name?: CountryCode
+  phone?: string
+}
+
+export type MondayListenResponse<T> = {
+  data: T,
+  method: string
+  requestId: string
+  type: string
+}
+
+export type SingleSettings = {
+  phoneColumnId: null | string
+}
+
+export type User = {
+  email: string
+  id: string,
+  name: string,
+  photo_thumb: string
 }
