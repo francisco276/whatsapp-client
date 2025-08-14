2adc32c3c00235e9c3fdbdbf57cf86dc216b5a49
DaniPoot <danielpoot.uc2@gmail.com>
2025-08-14 10:09:32 -0600

    ✨ Add utility to improve css class validations

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,11 @@
 import { ColumnValue } from '@/types/monday'
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function getColumnById({ columnValues, columnId }: { columnValues: ColumnValue[], columnId: string }) {
   return columnValues.find(column => column.id === columnId)
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
