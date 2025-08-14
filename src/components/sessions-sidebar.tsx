82ad901f943b41da995da279fcdcdb6e210c3a59
DaniPoot <danielpoot.uc2@gmail.com>
2025-08-14 11:00:22 -0600

    ♻️ Update component with new ui and small version

--- src/components/sessions-sidebar.tsx
+++ src/components/sessions-sidebar.tsx
@@ -1,62 +1,83 @@
-import { useState, useContext } from 'react'
-
-import { SessionContext } from './providers/session/session-context'
-import { AddSession } from './add-session'
-import { SessionButton } from './session-button'
-
+import { ERROR_LOAD_SESSIONS } from '@/config/errors'
+import { Session } from '@/types'
+import { cn } from '@/utils/utils'
+import { Box, Flex, Heading, IconButton } from '@vibe/core'
 import { NavigationChevronLeft, NavigationChevronRight, Settings } from '@vibe/icons'
-import { IconButton } from '@vibe/core'
-import { SideBarList } from './skeletons/sidebar-list'
+import { useCallback, useContext, useMemo, useState } from 'react'
 import { MondayApi } from '../lib/monday/api'
-
-type Session = {
-  id: string,
-  isSynced: boolean
-}
+import { AddSession } from './add-session'
+import { Error } from './error'
+import { SessionContext } from './providers/session/session-context'
+import { SessionsList } from './sessions/sessions-list'
+import { SideBarList } from './skeletons/sidebar-list'
 
 type SessionSidebarProps = {
-  sessions: Session[],
-  isLoading?: boolean
-  error?: boolean
+  type?: 'small' | 'full'
+  sessions: Session[]
+  loading: boolean
+  error: boolean
 }
 
-const SessionSidebar = ({ sessions = [], isLoading = false, error }: SessionSidebarProps) => {
+const SessionSidebar = ({ sessions, loading, type = 'full', error }: SessionSidebarProps) => {
   const monday = new MondayApi()
   const { session: currentSession, setSession } = useContext(SessionContext)
   const [sessionsSidebarOpen, setSessionsSidebarOpen] = useState(true)
 
-  function handleOpenModal() {
+  const isSmallVersion = useMemo(() => type === 'small', [type])
+
+  const isToggle = isSmallVersion ? false : sessionsSidebarOpen
+
+  const handleOpenModal = useCallback(() => {
     monday.execute("openAppFeatureModal", { urlPath: '/config', width: 900, height: 700 })
-  }
+  }, [])
 
-  return <div className={`transition-all duration-300 flex flex-col ${sessionsSidebarOpen ? 'w-64' : 'w-16'
-    }`}>
-    <div className="p-4 flex items-center">
-      <h2 className={`font-bold ${!sessionsSidebarOpen && 'hidden'}`}>Cuentas</h2>
-      {sessionsSidebarOpen && <IconButton className="ml-auto" size='small' icon={Settings} onClick={handleOpenModal} />}
-      <IconButton
-        onClick={() => setSessionsSidebarOpen(!sessionsSidebarOpen)}
-        size='small'
-        ariaLabel={sessionsSidebarOpen ? 'Cerrar' : 'Abrir'}
-        icon={sessionsSidebarOpen ? NavigationChevronLeft : NavigationChevronRight}
-      />
-    </div>
+  const handlerChange = useCallback((id: string) => {
+    setSession(id)
+  }, [])
 
-    <div className="p-2 flex-1 flex flex-col">
-      <AddSession disabled={isLoading || error} isToggle={sessionsSidebarOpen} />
-      {isLoading && <SideBarList />}
-      {!isLoading && sessions.map(session => (
-        <SessionButton
-          sessionId={session.id}
-          key={session.id}
-          onClick={() => setSession(session.id)}
-          isSelected={currentSession === session.id}
-          isSynced={session.isSynced}
-          isToggle={sessionsSidebarOpen}
-        />
-      ))}
-    </div>
-  </div>
+  return (
+    <Box className={cn(
+      'bg-white transition-all!',
+      {
+        'w-[137px] min-w-[137px] max-w-[137px]': isSmallVersion,
+        'w-xs! min-w-xs! max-w-xs!': !isSmallVersion && sessionsSidebarOpen,
+        'w-[100px]! min-w-[100px]! max-w-[100px]!': !isSmallVersion && !sessionsSidebarOpen,
+      }
+    )}>
+      <Flex direction='column' gap={20}>
+        <Box className='w-full bg-gradient-to-r from-slate-800 to-slate-900' padding="medium">
+          <Flex direction='column' align='start' gap={10}>
+            {
+              !isSmallVersion && <Flex gap={10} className='ml-auto transition-all duration-300'>
+                <IconButton size='small' kind='tertiary' color='fixed-light' icon={Settings} onClick={handleOpenModal} />
+                {!sessionsSidebarOpen && <IconButton size='small' kind='tertiary' color='fixed-light' icon={NavigationChevronLeft} onClick={() => setSessionsSidebarOpen(true)} />}
+                {sessionsSidebarOpen && <IconButton size='small' kind='tertiary' color='fixed-light' icon={NavigationChevronRight} onClick={() => setSessionsSidebarOpen(false)} />}
+              </Flex>
+            }
+            <Heading
+              type='h2'
+              weight='bold'
+              color='fixedLight'
+              className={cn({ 'invisible!': !isSmallVersion && !sessionsSidebarOpen })}
+            >
+              Sesiones
+            </Heading>
+            {!isSmallVersion && <AddSession disabled={loading || error} isToggle={sessionsSidebarOpen} />}
+          </Flex>
+        </Box>
+      </Flex>
+      {loading && <SideBarList />}
+      {error && <Error title={ERROR_LOAD_SESSIONS.title} errorMessage={ERROR_LOAD_SESSIONS.description} />}
+      <Box padding='medium' className='h-full'>
+        {(!loading && !error) && <SessionsList
+          sessions={sessions}
+          sessionSelected={currentSession}
+          onClickSession={handlerChange}
+          isToggle={isToggle}
+        />}
+      </Box>
+    </Box>
+  )
 }
 
 export { SessionSidebar }
