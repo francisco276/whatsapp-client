4079229f1859673b87e77bd278a42be6cf4739ee
DaniPoot <danielpoot.uc2@gmail.com>
2025-08-14 10:37:28 -0600

    ♻️ Refactor component to protect routes

--- src/components/with-authorization.tsx
+++ src/components/with-authorization.tsx
@@ -1,16 +1,20 @@
 import { useQuery } from '@tanstack/react-query'
 import { getIfAuthorizationUserExist } from '@/lib/services/authorization.ts'
-import { Flex } from '@vibe/core'
-import { Error } from './error'
+import { Flex, Icon } from '@vibe/core'
+import { Security } from '@vibe/icons'
 import { useWorkspaceId } from '@/hooks/useWorkspaceId'
+import { FullLoader } from './loading/full-loading'
+import { UserProvider } from './providers/user/user-provider'
+import { useUserId } from '@/hooks/useUserId'
 
 type WithAuthorizationProps = {
-  userId: string
   children: React.ReactNode
 }
 
-export const WithAuthorization = ({ userId, children }: WithAuthorizationProps) => {
+export const WithAuthorization = ({ children }: WithAuthorizationProps) => {
   const workspaceId = useWorkspaceId()
+  const userId = useUserId()
+
   const { data: isValidUser, isLoading, isFetched } = useQuery({
     queryKey: ['authorized', workspaceId, userId],
     queryFn: () => getIfAuthorizationUserExist({ workspaceId, userId }),
@@ -18,16 +22,25 @@ export const WithAuthorization = ({ userId, children }: WithAuthorizationProps) 
   })
 
   if (!isFetched || isLoading) {
-    return <p>Loading....</p>
+    return <FullLoader
+      title="Revisando tus permisos"
+      description='Solo estamos asegurándonos de que todo esté en orden antes de continuar.'
+    />
   }
 
   if (!isValidUser) {
     return (
-      <Flex className='w-screen h-screen' justify='center' align='center' >
-        <Error errorMessage="Tu cuenta no tiene los permisos necesarios para ver esta información." title='No tienes acceso a esta sección.' />
+      <Flex className='h-screen bg-slate-50'>
+        <Flex className='flex-1' justify='center' align='center' direction='column'>
+          <Icon iconSize={56} icon={Security} className='text-[#E83442]' />
+          <div className="text-center">
+            <h3 className="text-lg font-medium mb-2 text-slate-700 mt-4">Acceso denegado</h3>
+            <p className="text-sm text-slate-500">No tienes permiso para acceder a esta aplicación. Por favor, contacta a tu administrador.</p>
+          </div>
+        </Flex>
       </Flex>
     )
   }
 
-  return children
+  return <UserProvider userId={userId}>{children}</UserProvider>
 }
