311a5c799a3f2edd3949d655cc6b244eedfb464b
DaniPoot <danielpoot.uc2@gmail.com>
2025-08-14 10:42:33 -0600

    ♻️ Update component to improve ui design

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -1,4 +1,5 @@
-import { AttentionBox } from '@vibe/core'
+import { Flex, Icon } from '@vibe/core'
+import { Warning } from '@vibe/icons'
 import { DEFAULT_ERROR } from '../config/errors'
 
 type ErrorProps = {
@@ -7,11 +8,13 @@ type ErrorProps = {
 }
 
 export const Error = ({ title, errorMessage }: ErrorProps) => (
-  <div>
-    <AttentionBox
-      title={title || 'Application Error'}
-      text={errorMessage || DEFAULT_ERROR}
-      type={AttentionBox.types.DANGER}
-    />
-  </div>
-);
+  <Flex className='h-screen w-full bg-slate-50'>
+    <Flex className='flex-1' justify='center' align='center' direction='column'>
+      <Icon iconSize={56} icon={Warning} className='text-[#E83442]' />
+      <div className="text-center">
+        <h3 className="text-lg font-medium mb-2 text-slate-700 mt-4">{title}</h3>
+        <p className="text-sm text-slate-500">{errorMessage ?? DEFAULT_ERROR}</p>
+      </div>
+    </Flex>
+  </Flex>
+)
