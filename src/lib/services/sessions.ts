44cc1c81320fdbd8ed56fb62f426287c61df66c7
DaniPoot <danielpoot.uc2@gmail.com>
2025-08-14 11:31:52 -0600

    ♻️ Update to use new types

--- src/lib/services/sessions.ts
+++ src/lib/services/sessions.ts
@@ -1,17 +1,19 @@
+import { SuccessDataResponse } from "@/types/response"
 import { api } from "../axios"
+import { Session } from "@/types"
 
 const ROUTE = '/sessions'
 
 export const addSession = async ({ workspaceId, sessionId }: { workspaceId: string, sessionId: string }) => {
   try {
-    const response = await api.post(`${ROUTE}/add`, {
+    const { data: response } = await api.post<SuccessDataResponse<{ qr: string }>>(`${ROUTE}/add`, {
       workspaceId,
       sessionId
     }, {
       timeout: 0
     })
 
-    return response.data as { ok: boolean, message: string, qr: string }
+    return response.data
   } catch (error) {
     throw new Error('Error adding a new session')
   }
@@ -19,9 +21,8 @@ export const addSession = async ({ workspaceId, sessionId }: { workspaceId: stri
 
 export const getSessions = async ({ workspaceId }: { workspaceId: string }) => {
   try {
-    const response = await api.get(`${ROUTE}/${workspaceId}`)
-
-    return response.data as { sessions: { id: string, isSynced: boolean }[] }
+    const { data: response } = await api.get<SuccessDataResponse<{ sessions: Session[] }>>(`${ROUTE}/${workspaceId}`)
+    return response.data
   } catch (error) {
     throw new Error('Error on fetch sessions')
   }
@@ -29,14 +30,14 @@ export const getSessions = async ({ workspaceId }: { workspaceId: string }) => {
 
 export const deleteSession = async ({ workspaceId, sessionId }: { workspaceId: string, sessionId: string }) => {
   try {
-    const response = await api.delete(`${ROUTE}`, {
+    const response = await api.delete<SuccessDataResponse<{ sessions: Session[] }>>(`${ROUTE}`, {
       data: {
         workspaceId,
         sessionId
       }
     })
 
-    return response.data as { sessions: { id: string }[] }
+    return response.data
   } catch (error) {
     throw new Error('Error on delete sessions')
   }
