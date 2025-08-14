0850aeb8694f7cd07b3aa0c29414f6bec895d357
DaniPoot <danielpoot.uc2@gmail.com>
2025-08-14 11:06:41 -0600

    ♻️ Update component to only use for selection chat

--- src/components/chats-sidebar.tsx
+++ src/components/chats-sidebar.tsx
@@ -1,58 +1,53 @@
-import { useState, useContext } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { SessionContext } from './providers/session/session-context'
-
-import { IconButton, AttentionBox } from '@vibe/core'
-import { NavigationChevronLeft, NavigationChevronRight, Info } from '@vibe/icons'
-
-import { getChats } from '../lib/services/chats'
+import { useState } from 'react'
+import { IconButton, Flex, Box, Heading } from '@vibe/core'
+import { NavigationChevronLeft, NavigationChevronRight } from '@vibe/icons'
 import { SideBarList } from './skeletons/sidebar-list'
 import { ChatList } from './list/chats-list'
 import { Error } from './error'
-import { ERROR_SERVER_ERROR } from '../config/errors'
-import { useRegisterNewMessage } from '@/hooks/useRegisterNewMessage'
+import { ERROR_LOAD_CONTACT } from '../config/errors'
+import { cn } from '@/utils/utils'
+import { Chat } from '@/lib/services/chats'
 
-type ChatsSidebarProps = {
-  workspaceId: string
+type ChatSidebarProps = {
+  chats: Chat[]
+  loading: boolean
+  error: boolean
 }
 
-const ChatsSidebar = ({ workspaceId }: ChatsSidebarProps) => {
-  const { session } = useContext(SessionContext)
+const ChatsSidebar = ({ chats, loading, error }: ChatSidebarProps) => {
   const [chatsSidebarOpen, setChatsSidebarOpen] = useState(true)
 
-  const { data, isLoading, isSuccess, isPending, isError } = useQuery({
-    queryKey: ['getChats', session],
-    queryFn: () => getChats({ workspaceId, sessionId: session }),
-    enabled: !!session
-  })
-
-  useRegisterNewMessage({ workspaceId, chats: data?.chats })
-
   return (
-    <div className={`bg-gray-100 border-r border-r-gray-200 transition-all duration-300 flex flex-col ${chatsSidebarOpen ? 'w-70' : 'w-20'}`}>
-      <div className="p-4 flex justify-between items-center">
-        <h2 className={`font-bold ${!chatsSidebarOpen && 'hidden'}`}>Chats</h2>
-        <IconButton
-          size='small'
-          onClick={() => setChatsSidebarOpen(!chatsSidebarOpen)}
-          ariaLabel={chatsSidebarOpen ? 'Cerrar' : 'Abrir'}
-          icon={chatsSidebarOpen ? NavigationChevronLeft : NavigationChevronRight}
-          disabled={isError}
-        />
-      </div>
-      <div className="p-2 flex-1 flex">
-        {isPending && chatsSidebarOpen && (
-          <AttentionBox
-            title="Selecciona una cuenta"
-            text="Debes elegir una cuenta para ver o listar los chats disponibles."
-            icon={Info}
-          />
-        )}
-        {isLoading && <SideBarList />}
-        {isError && <Error errorMessage={ERROR_SERVER_ERROR} />}
-        {isSuccess && <ChatList chats={data.chats} isToggle={chatsSidebarOpen} workspaceId={workspaceId} />}
-      </div>
-    </div>
+    <Box
+      className={cn(
+        'bg-white border-slate-200! transition-all!',
+        chatsSidebarOpen ? 'w-xs! min-w-xs! max-w-xs!' : 'w-[100px]! min-w-[100px]! max-w-[100px]!'
+      )}
+      border
+    >
+      <Flex direction='column' className='h-screen'>
+        <Box className='w-full border-b! border-x-0 border-slate-200! h-[146px] min-h-[146px]' padding="medium">
+          <Flex direction='column' align='start' gap={10}>
+            <Flex gap={10} className='ml-auto transition-all duration-300'>
+              {!chatsSidebarOpen && <IconButton size='small' kind='tertiary' icon={NavigationChevronLeft} onClick={() => setChatsSidebarOpen(true)} />}
+              {chatsSidebarOpen && <IconButton size='small' kind='tertiary' icon={NavigationChevronRight} onClick={() => setChatsSidebarOpen(false)} />}
+            </Flex>
+            <Heading type='h2' weight='bold' className={`${chatsSidebarOpen ? '' : 'invisible!'}`}>Chats</Heading>
+          </Flex>
+        </Box>
+        {error && <Error title={ERROR_LOAD_CONTACT.title} errorMessage={ERROR_LOAD_CONTACT.title} />}
+        {loading && <SideBarList />}
+        <Box className='flex-1! w-full flex justify-start'>
+          {(!loading && !error) && (
+            <ChatList
+              chats={chats}
+              isToggle={chatsSidebarOpen}
+              workspaceId={'1234'} />
+          )
+          }
+        </Box>
+      </Flex>
+    </Box>
   )
 }
 
