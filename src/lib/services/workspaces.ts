b06361786d5c4953a98b80fffff4d8b1929ae791
DaniPoot <danielpoot.uc2@gmail.com>
2025-09-22 11:43:59 -0600

    ⚰️ Remove unnused code

--- src/lib/services/workspaces.ts
+++ src/lib/services/workspaces.ts
@@ -10,23 +10,17 @@ export const addWorkspace = async ({ workspaceId, name }: { workspaceId: string,
     })
 
     return response.data as { ok: boolean, message: string }
-  } catch (error) {
+  } catch {
     throw new Error('Error adding a new workspace')
   }
 }
 
 export const getWorkspace = async ({ workspaceId }: { workspaceId: string }) => {
   try {
-    // if (workspaceId === undefined) {
-    //   throw new Error('Workspace ID is required')
-    // }
     const response: { ok?: boolean, data: { data?: string, name?: string, error?: string } } = await api.get(`${ROUTE}/${workspaceId}`)
-    if (response.data.error) {
-      throw new Error('Error on fetch workspaces')
-    }
 
     return response.data as { id: string, name: string }
-  } catch (error) {
+  } catch {
     throw new Error('Error on fetch workspaces')
   }
 }
