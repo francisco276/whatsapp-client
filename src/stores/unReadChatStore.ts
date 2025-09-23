548330530377c522fa85d4f75aa4681f966f525d
DaniPoot <danielpoot.uc2@gmail.com>
2025-09-23 13:10:51 -0600

    ✨ Add a new function to set initial data

--- src/stores/unReadChatStore.ts
+++ src/stores/unReadChatStore.ts
@@ -2,12 +2,14 @@ import { create } from 'zustand'
 
 type UnreadChatStoreState = {
   contacts: { [id: string]: number }
+  setInitialData: (id: string, value: number) => void
   incrementUnreadChat: (id: string) => void
   resetUnreadChat: (id: string) => void
 }
 
 export const unreadChatStore = create<UnreadChatStoreState>()((set) => ({
   contacts: {},
+  setInitialData: (id: string, value: number) => set((state) => ({ contacts: { ...state.contacts, [id]: value } })),
   incrementUnreadChat: (id: string) => set((state) => ({ contacts: { ...state.contacts, [id]: (state.contacts[id] || 0) + 1 } })),
   resetUnreadChat: (id: string) => set((state) => ({ contacts: { ...state.contacts, [id]: 0 } }))
 }))
