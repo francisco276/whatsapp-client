c51ad65b4fc317ad77937372438c992184a7889b
DaniPoot <danielpoot.uc2@gmail.com>
2025-10-01 11:45:00 -0600

    🔥 Remove old section

--- src/components/settings/account-section.tsx
+++ src/components/settings/account-section.tsx
@@ -1,5 +1,4 @@
 import { Box, Heading } from '@vibe/core'
-import { RemoveSession } from './remove-session'
 import { Authorization } from './authorization'
 import { Notifications } from './notifications'
 
@@ -7,7 +6,6 @@ export const AccountSection = () => {
   return (
     <Box>
       <Heading type="h1" weight="bold" className='!mb-px'>Account</Heading>
-      <RemoveSession />
       <Notifications />
       <Authorization />
     </Box>
