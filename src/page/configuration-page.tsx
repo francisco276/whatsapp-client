9664274811f91a244dd0cbdfbd16c544f5c17712
DaniPoot <danielpoot.uc2@gmail.com>
2025-10-01 11:33:13 -0600

    ✨ Add new section for sessions

--- src/page/configuration-page.tsx
+++ src/page/configuration-page.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import { Box, Flex, Heading, Icon, Menu, MenuItem, Divider } from '@vibe/core'
-import { Settings, Person } from '@vibe/icons'
+import { Settings, Person, Basic } from '@vibe/icons'
 import { useContext } from '../hooks/useContext.ts'
 import { MondayApi } from '../lib/monday/api'
 import { AccountSection } from '@/components/settings/account-section'
 import { WorkspaceProvider } from '@/components/providers/workspace/workspace-provider'
+import { SessionsSection } from '@/components/settings/sessions-settings.tsx'
 
 const ConfigurationPage = () => {
   const monday = new MondayApi()
@@ -17,9 +18,9 @@ const ConfigurationPage = () => {
       <Box
         className="h-screen w-screen"
       >
-        <Flex>
+        <Flex align='start' className='h-full'>
           <Box
-            className="max-w-[300px] h-screen"
+            className="w-[300px] max-w-[300px] h-full"
             backgroundColor="allgreyBackgroundColor"
             padding="large"
           >
@@ -29,15 +30,17 @@ const ConfigurationPage = () => {
             </Flex>
             <Divider />
             <Menu>
-              <MenuItem icon={Person} title="Account" onClick={() => setPage('account')} />
+              <MenuItem icon={Person} title="Cuenta" onClick={() => setPage('account')} />
+              <MenuItem icon={Basic} title="Sesiones" onClick={() => setPage('sessions')} />
             </Menu>
           </Box>
           <Box
-            className="w-[50%] h-screen"
+            className="w-full h-full"
             padding="large"
           >
             <WorkspaceProvider workspaceId={workspaceId}>
               {page === 'account' && <AccountSection />}
+              {page === 'sessions' && <SessionsSection />}
             </WorkspaceProvider>
           </Box>
         </Flex>
