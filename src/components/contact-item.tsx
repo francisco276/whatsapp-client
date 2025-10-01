8224d178fe1d487ad8ab36ca45abf810eaa22375
DaniPoot <danielpoot.uc2@gmail.com>
2025-10-01 11:35:24 -0600

    ♻️ Update to use new hook for reusability

--- src/components/contact-item.tsx
+++ src/components/contact-item.tsx
@@ -1,38 +1,26 @@
-import { useQuery } from '@tanstack/react-query'
-import { SessionContext } from './providers/session/session-context'
 import { ChatContext } from './providers/chat/chat-context'
-import { getContact } from '../lib/services/contacts'
 import { Button, Avatar, Skeleton, Badge, Flex, Text } from '@vibe/core'
 import { Comment } from '@vibe/icons'
 import { useMemo, useContext } from 'react'
 import { unreadChatStore } from '@/stores/unReadChatStore'
-import { jidToFormatedPhone } from '@/utils/whatsapp'
+import { useGetContact } from '@/hooks/useGetContact'
 
 type ContactItemProps = {
   contactId: string
   isToggle: boolean
-  workspaceId: string
 }
 
-export const ContactItem = ({ contactId, isToggle, workspaceId }: ContactItemProps) => {
+export const ContactItem = ({ contactId, isToggle }: ContactItemProps) => {
   const unread = unreadChatStore((state) => state.contacts[contactId])
-  const { session: sessionId } = useContext(SessionContext)
   const { chat, setChat } = useContext(ChatContext)
-  const { data: contact, isLoading } = useQuery({
-    queryKey: ['getContact', contactId],
-    queryFn: () => getContact({ id: contactId, workspaceId, sessionId }),
-    refetchOnWindowFocus: false,
-    staleTime: 1440 * 60 * 1000
-  })
+  const { query, contact } = useGetContact({ contactId })
+  const { isLoading } = query
 
   const isSelected = useMemo(() => chat === contactId, [chat, contactId])
 
   if (isLoading) {
     return <Skeleton fullWidth={true} height={48} type='rectangle' />
   }
-
-  const { image, name, verifiedName, notify, id } = contact
-  const displayName = name || verifiedName || notify || jidToFormatedPhone(id)
 
   const button = <Button
     className={`w-full border-1! border-[#A3E7F3]! ${isToggle ? 'justify-start!' : ''}  ${isSelected ? 'bg-[#E8F9FC]!' : ''}`}
@@ -44,8 +32,8 @@ export const ContactItem = ({ contactId, isToggle, workspaceId }: ContactItemPro
   >
     <Flex gap={10} justify='start'>
       {isLoading && <Skeleton type="circle" size="p" />}
-      {!isLoading && (image ? <Avatar size="small" type="img" src={image} /> : <Avatar size='small' type="icon" icon={Comment} withoutBorder={true} />)}
-      {(!isLoading && isToggle && displayName) && <Text type='text1' maxLines={1}>{displayName}</Text>}
+      {!isLoading && (contact?.image ? <Avatar size="small" type="img" src={contact?.image} /> : <Avatar size='small' type="icon" icon={Comment} withoutBorder={true} />)}
+      {(!isLoading && isToggle && contact?.displayName) && <Text type='text1' maxLines={1}>{contact?.displayName}</Text>}
     </Flex>
   </Button>
 
