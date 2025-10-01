c7d1c7fa7f68251790b6cb471232eb8d3fce0b30
DaniPoot <danielpoot.uc2@gmail.com>
2025-10-01 11:16:27 -0600

    ♻️ Update item to select the role of the user

--- src/components/user/user-item.tsx
+++ src/components/user/user-item.tsx
@@ -1,7 +1,9 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { ListItem, ListItemAvatar, Label, Text } from '@vibe/core'
+import { ListItem, ListItemAvatar, Label, Text, Dropdown } from '@vibe/core'
 import { updateAuthorizationUser } from '@/lib/services/authorization'
 import { AuthorizedUser } from '@/types'
+import { USER_TYPES } from '@/config/constants'
+import { useMemo } from 'react'
 
 type UserItemProps = {
   user: AuthorizedUser
@@ -9,12 +11,14 @@ type UserItemProps = {
 
 export const UserItem = ({ user }: UserItemProps) => {
   const queryClient = useQueryClient()
-  const { mutate } = useMutation({
+  const { mutate, isPending } = useMutation({
     mutationFn: updateAuthorizationUser,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['authorizeUsers'] })
     }
   })
+
+  const options = useMemo(() => Object.entries(USER_TYPES).map(([key, value]) => ({ label: value, value: key })), [])
 
   function handleUpdate() {
     const payload = {
@@ -24,11 +28,34 @@ export const UserItem = ({ user }: UserItemProps) => {
     }
     mutate(payload)
   }
+
+  function handlerRole({ value: role }: { value: keyof typeof USER_TYPES, label: string }) {
+    const payload = {
+      workspaceId: user.workspaceId,
+      userId: user.id,
+      role
+    }
+    mutate(payload)
+  }
   return (
-    <ListItem size="large" className="!px-0" component="div" onClick={handleUpdate}>
+    <ListItem size="large" className="!px-0 overflow-visible!" component="div" >
       <ListItemAvatar src={user.image} />
-      <Text ellipsis>{user.name}</Text>
-      <Label className="ml-auto" text="Autorizado" color="positive" labelClassName={!user.authorized ? "!bg-neutral-300" : ""} />
+      <Text className="mr-auto" ellipsis>{user.name}</Text>
+      {
+        user.authorized && (
+          <Dropdown
+            clearable={false}
+            size="small"
+            className='flex-1 max-w-[140px] mr-2'
+            searchable={false}
+            options={options}
+            disabled={isPending}
+            value={options.find(({ value }) => value === user.role)}
+            onChange={handlerRole}
+          />
+        )
+      }
+      <Label text="Autorizado" color="positive" labelClassName={!user.authorized ? "!bg-neutral-300" : ""} onClick={handleUpdate} />
     </ListItem>
   )
 }
