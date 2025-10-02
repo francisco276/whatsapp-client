cddbf91bd712219e3c6437f1b7b8ba8218ecb86b
DaniPoot <danielpoot.uc2@gmail.com>
2025-10-02 10:22:27 -0600

    ♻️ Update sytles and functionality on add session button

--- src/components/add-session.tsx
+++ src/components/add-session.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState, useMemo } from 'react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { addSession } from '../lib/services/sessions'
-
-import { Button, Flex, Skeleton, Toast, ToastType } from "@vibe/core"
+import { Button, Flex, Loader, Toast, ToastType } from "@vibe/core"
 import { Modal, ModalHeader, ModalContent, ModalFooter, ModalBasicLayout } from '@vibe/core/next'
 import { Add } from '@vibe/icons'
 import { SocketClient } from '../lib/socket'
 import { handlerUpdateQrCode, handlerConnection } from '../lib/socket-handlers/session'
 import { useWorkspaceId } from '@/hooks/useWorkspaceId'
+import { Error } from './error'
+import { ERROR_LOAD_QR } from '@/config/errors'
 
 export const AddSession = ({ isToggle, disabled }: { isToggle: boolean, disabled?: boolean }) => {
   const queryClient = useQueryClient()
@@ -16,14 +17,25 @@ export const AddSession = ({ isToggle, disabled }: { isToggle: boolean, disabled
   const [qrCode, setQrCode] = useState<string | null>()
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [toast, setToast] = useState({ type: '', message: '', })
-  const { data, isIdle, mutate, isPending: isLoading } = useMutation({
-    mutationFn: addSession
+  const { data, isIdle, mutate, isPending: isLoading, isError, reset } = useMutation({
+    mutationKey: ['AddSession'],
+    mutationFn: addSession,
+    onSuccess: (data) => {
+      setQrCode(data.qr)
+    },
+    gcTime: 0
   })
 
-  const handleClose = () => setIsOpen(false)
+  const handleClose = () => {
+    setIsOpen(false)
+    reset()
+    setQrCode(null)
+  }
   const handleOpen = () => {
     const sessionId = Date.now().toString()
+    setQrCode(null)
     setIsOpen(true)
+    reset()
     if (isIdle) {
       mutate({ workspaceId, sessionId })
     }
@@ -65,13 +77,14 @@ export const AddSession = ({ isToggle, disabled }: { isToggle: boolean, disabled
   return (
     <>
       <Button
-        kind='tertiary'
+        size='small'
+        kind='primary'
         leftIcon={Add}
-        className='w-full'
+        className='w-full bg-[#0DACC8]! text-white hover:bg-[#0B8AA0]!'
         onClick={handleOpen}
         disabled={disabled}
       >
-        {isToggle && <span>New Session</span>}
+        {isToggle && <span>Nueva sesion</span>}
       </Button>
       <Modal
         id="modal-basic-small"
@@ -82,18 +95,24 @@ export const AddSession = ({ isToggle, disabled }: { isToggle: boolean, disabled
       >
         <ModalBasicLayout>
           <ModalHeader
-            title="Escanee el código QR para añadir una cuenta"
+            title="Escanea el código QR"
           />
           <ModalContent>
             <Flex align='center' justify='center'>
-              {isLoading || (!isLoading && !qrCode) ? <Skeleton /> : <img src={qr} />}
+              {!isError && (isLoading || (!isLoading && !qrCode)) ? <Loader size='large' className='mx-auto text-[#0DACC8]' hasBackground /> : <img src={qr} />}
             </Flex>
+            {isError && <Error errorMessage={ERROR_LOAD_QR.description} />}
           </ModalContent>
         </ModalBasicLayout>
         <ModalFooter
+          secondaryButton={{
+            text: 'Cancelar',
+            onClick: handleClose
+          }}
           primaryButton={{
             text: "Cerrar",
             onClick: handleClose,
+            className: 'bg-[#0DACC8]! text-white hover:bg-[#0B8AA0]!'
           }}
         />
       </Modal>
