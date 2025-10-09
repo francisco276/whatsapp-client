ebcb99829d169a014eb62123d5c5d02e2961add9
DaniPoot <danielpoot.uc2@gmail.com>
2025-10-09 12:41:34 -0600

    ♻️ Update to use new components

--- src/page/home-page.tsx
+++ src/page/home-page.tsx
@@ -1,13 +1,19 @@
 import Sessions from "@/components/layout/sessions"
 import Workspace from "@/components/layout/workspace"
 import Chats from "@/components/layout/chats"
+import Authorization from "@/components/layout/authorization"
+import MondayContex from "@/components/layout/monday-context";
 
 export default function HomePage() {
   return (
-    <Workspace>
-      <Sessions>
-        <Chats />
-      </Sessions>
-    </Workspace>
+    <MondayContex>
+      <Authorization>
+        <Workspace>
+          <Sessions>
+            <Chats />
+          </Sessions>
+        </Workspace>
+      </Authorization>
+    </MondayContex>
   )
 }
