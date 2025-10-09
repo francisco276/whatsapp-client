09e876470d65047c2e824c45d8ab145c20a8062b
DaniPoot <danielpoot.uc2@gmail.com>
2025-10-09 13:42:20 -0600

    ♻️ Add new components to get initial data

--- src/page/templates-page.tsx
+++ src/page/templates-page.tsx
@@ -4,59 +4,65 @@ import { Heading, Box, Text, Flex, Button, useSwitch } from "@vibe/core"
 import { Modal, ModalHeader, ModalContent, ModalBasicLayout } from '@vibe/core/next'
 import { Add } from '@vibe/icons'
 import { Templates } from "@/components/layout/templates"
+import Authorization from "@/components/layout/authorization"
+import MondayContex from "@/components/layout/monday-context";
 
 export default function TemplatesPage() {
   const { isChecked, onChange } = useSwitch()
 
   return (
-    <Workspace>
-      <Box padding="xxxl">
-        <Box>
-          <Heading className="font-medium! text-slate-700!">Plantillas de Mensajes</Heading>
-          <Text className="text-sm! text-slate-500!">Gestiona y organiza todas tus plantillas de mensajes</Text>
-        </Box>
-        <Box>
-          <Flex justify="end">
-            <Button
-              kind='primary'
-              leftIcon={Add}
-              className='bg-[#0DACC8]! text-white hover:bg-[#0B8AA0]!'
-              onClick={onChange}
-            >
-              Nueva plantilla
-            </Button>
-          </Flex>
-        </Box>
-        <Box paddingY="xl">
-          <Templates />
-        </Box>
-      <Modal
-        id="modal-basic-small"
-        show={isChecked}
-        size="medium"
-        onClose={onChange}
-        alertModal
-      >
-        <ModalBasicLayout>
-          <ModalHeader
-            title="Crear nueva plantilla"
-            description="Complete el formulario para crear una nueva plantilla"
-          />
-          <ModalContent>
-              <TemplateForm
-                cancelButton={{
-                  text: 'Cancelar',
-                  onClick: onChange
-                }}
-                submitButton={{
-                  text: 'Crear template'
-                }}
-                onDonde={onChange}
-              />
-          </ModalContent>
-        </ModalBasicLayout>
-      </Modal>
-      </Box>
-    </Workspace>
+    <MondayContex>
+      <Authorization>
+        <Workspace>
+              <Box padding="xxxl">
+                <Box>
+                  <Heading className="font-medium! text-slate-700!">Plantillas de Mensajes</Heading>
+                  <Text className="text-sm! text-slate-500!">Gestiona y organiza todas tus plantillas de mensajes</Text>
+                </Box>
+                <Box>
+                  <Flex justify="end">
+                    <Button
+                      kind='primary'
+                      leftIcon={Add}
+                      className='bg-[#0DACC8]! text-white hover:bg-[#0B8AA0]!'
+                      onClick={onChange}
+                    >
+                      Nueva plantilla
+                    </Button>
+                  </Flex>
+                </Box>
+                <Box paddingY="xl">
+                  <Templates />
+                </Box>
+              <Modal
+                id="modal-basic-small"
+                show={isChecked}
+                size="medium"
+                onClose={onChange}
+                alertModal
+              >
+                <ModalBasicLayout>
+                  <ModalHeader
+                    title="Crear nueva plantilla"
+                    description="Complete el formulario para crear una nueva plantilla"
+                  />
+                  <ModalContent>
+                      <TemplateForm
+                        cancelButton={{
+                          text: 'Cancelar',
+                          onClick: onChange
+                        }}
+                        submitButton={{
+                          text: 'Crear template'
+                        }}
+                        onDonde={onChange}
+                      />
+                  </ModalContent>
+                </ModalBasicLayout>
+              </Modal>
+              </Box>
+            </Workspace>
+      </Authorization>
+    </MondayContex>
   )
 }
