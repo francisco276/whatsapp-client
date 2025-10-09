e23872b4426166a56238dcfcd2f8c5b38350318c
DaniPoot <danielpoot.uc2@gmail.com>
2025-10-09 16:07:00 -0600

    🐛 Fix bug with web sockets

--- src/hooks/useRegisterNewMessage.ts
+++ src/hooks/useRegisterNewMessage.ts
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo } from 'react'
+import { useContext, useEffect } from 'react'
 import { SessionContext } from '@/components/providers/session/session-context'
 import { ChatContext } from '@/components/providers/chat/chat-context'
 import { SocketClient } from '@/lib/socket'
@@ -6,24 +6,26 @@ import { handlerNotifyMessage } from '@/lib/socket-handlers/messages'
 import { unreadChatStore } from '@/stores/unReadChatStore'
 import { Chat } from '@/lib/services/chats'
 
-
 export const useRegisterNewMessage = ({ workspaceId, chats }: { workspaceId: string, chats?: Chat[] }) => {
   const { session } = useContext(SessionContext)
   const { chat } = useContext(ChatContext)
   const incrementUnreadChat = unreadChatStore((state) => state.incrementUnreadChat)
   const setInitialData = unreadChatStore((state) => state.setInitialData)
 
-  const socket = useMemo(() => new SocketClient({ workspaceId, sessionId: session }), [workspaceId, session])
-
   useEffect(() => {
+    console.log({ workspaceId, session})
+    if (!workspaceId || !session) return
+
+    const socket = new SocketClient({ workspaceId, sessionId: session })
+
     handlerNotifyMessage(socket, ({ id, unreadCount }) => {
       if (chat !== id && unreadCount) incrementUnreadChat(id)
     })
-
-    return (() => {
-      socket.disconnect()
-    })
-  }, [socket])
+ 
+    return () => {
+      socket?.disconnect()
+    }
+  }, [workspaceId, session])
 
   chats?.forEach((chat) => {
     if (chat.unreadCount) {
