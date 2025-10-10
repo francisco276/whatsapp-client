25e8a647a7c8d28e333d75ae84a0df92c93515b1
DaniPoot <danielpoot.uc2@gmail.com>
2025-10-09 18:01:14 -0600

    ♻️ Update code to use new components

--- src/page/configuration-page.tsx
+++ src/page/configuration-page.tsx
@@ -1,51 +1,49 @@
 import { useState } from 'react'
 import { Box, Flex, Heading, Icon, Menu, MenuItem, Divider } from '@vibe/core'
 import { Settings, Person, Basic } from '@vibe/icons'
-import { useContext } from '../hooks/useContext.ts'
-import { MondayApi } from '../lib/monday/api'
 import { AccountSection } from '@/components/settings/account-section'
-import { WorkspaceProvider } from '@/components/providers/workspace/workspace-provider'
 import { SessionsSection } from '@/components/settings/sessions-settings.tsx'
+import Authorization from "@/components/layout/authorization"
+import MondayContex from "@/components/layout/monday-context";
 
 const ConfigurationPage = () => {
-  const monday = new MondayApi()
-  const { context } = useContext({ monday })
-  const { accountId: workspaceId } = context
   const [page, setPage] = useState('account')
 
   return (
-    <div>
-      <Box
-        className="h-screen w-screen"
-      >
-        <Flex align='start' className='h-full'>
+    <MondayContex>
+      <Authorization>
+        <div>
           <Box
-            className="w-[300px] max-w-[300px] h-full"
-            backgroundColor="allgreyBackgroundColor"
-            padding="large"
+            className="h-screen w-screen"
           >
-            <Flex gap={10} align='center'>
-              <Icon icon={Settings} />
-              <Heading type="h3" weight="bold">Settings</Heading>
+            <Flex align='start' className='h-full'>
+              <Box
+                className="w-[300px] max-w-[300px] h-full"
+                backgroundColor="allgreyBackgroundColor"
+                padding="large"
+              >
+                <Flex gap={10} align='center'>
+                  <Icon icon={Settings} />
+                  <Heading type="h3" weight="bold">Settings</Heading>
+                </Flex>
+                <Divider />
+                <Menu>
+                  <MenuItem icon={Person} title="Cuenta" onClick={() => setPage('account')} />
+                  <MenuItem icon={Basic} title="Sesiones" onClick={() => setPage('sessions')} />
+                </Menu>
+              </Box>
+              <Box
+                className="w-full h-full"
+                padding="large"
+              >
+                {page === 'account' && <AccountSection />}
+                {page === 'sessions' && <SessionsSection />}
+              </Box>
             </Flex>
-            <Divider />
-            <Menu>
-              <MenuItem icon={Person} title="Cuenta" onClick={() => setPage('account')} />
-              <MenuItem icon={Basic} title="Sesiones" onClick={() => setPage('sessions')} />
-            </Menu>
           </Box>
-          <Box
-            className="w-full h-full"
-            padding="large"
-          >
-            <WorkspaceProvider workspaceId={workspaceId}>
-              {page === 'account' && <AccountSection />}
-              {page === 'sessions' && <SessionsSection />}
-            </WorkspaceProvider>
-          </Box>
-        </Flex>
-      </Box>
-    </div>
+        </div>
+      </Authorization>
+    </MondayContex>
   )
 }
 
