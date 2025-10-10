0778e63cedbb24a99a268ecc1c7c4b5173472300
DaniPoot <danielpoot.uc2@gmail.com>
2025-10-09 18:42:39 -0600

    ♻️ Update component for better usability and readbility

--- src/page/single-chat-page.tsx
+++ src/page/single-chat-page.tsx
@@ -1,27 +1,21 @@
 import { useState, useEffect, useMemo } from "react"
-import { useQuery } from "@tanstack/react-query"
-import { useContext as useMondayContex } from "@/hooks/useContext"
 import { MondayApi } from '@/lib/monday/api'
-import { ERROR_LOAD_MESSAGES_HISTORY, ERROR_PHONE_COLUMN_CONFIGURATION } from "@/config/errors"
+import { ERROR_PHONE_COLUMN_CONFIGURATION } from "@/config/errors"
 import { Error } from "@/components/error"
-import { getSingleChatInformation } from "@/lib"
 import { SingleSettings } from "@/types/monday"
-import { EmptyState } from "@/components/empty-state"
-import { FullLoader } from "@/components/loading/full-loading"
 import Workspace from "@/components/layout/workspace"
 import Sessions from "@/components/layout/sessions"
-import Chats from "@/components/layout/chats"
+import { SingleChat } from "@/components/layout/single-chat"
+import MondayContex from "@/components/layout/monday-context";
+import Authorization from "@/components/layout/authorization"
 
 const SingleChatPage = () => {
   const [state, setState] = useState({
     phoneColumnId: '',
-    error: '',
-    sessionId: ''
+    error: ''
   })
 
   const monday = useMemo(() => new MondayApi(), [])
-  const { context } = useMondayContex({ monday })
-  const { itemId, accountId: workspaceId } = useMemo(() => context, [context])
 
   useEffect(() => {
     monday.listen<SingleSettings>('settings', (data) => {
@@ -32,40 +26,23 @@ const SingleChatPage = () => {
         setState(value => ({ ...value, error: ERROR_PHONE_COLUMN_CONFIGURATION }))
       }
     })
-  }, [])
-
-
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['getItem', itemId],
-    queryFn: () => getSingleChatInformation({
-      monday,
-      workspaceId,
-      sessionId: state.sessionId,
-      itemId,
-      phoneColumnId: state.phoneColumnId
-    }),
-    enabled: !!state.sessionId,
-  })
+  }, [monday])
 
   if (state.error) {
     return <Error errorMessage={state.error} />
   }
 
   return (
-    <Workspace>
-      <Sessions type="small">
-        {isError && <Error title={ERROR_LOAD_MESSAGES_HISTORY.title} errorMessage={ERROR_LOAD_MESSAGES_HISTORY.description} />}
-        {isLoading && <FullLoader title="Recuperando historial de conversación" description="Estamos recuperando el historial de conversación. Esto puede tardar unos segundos." />}
-        {
-          (!isError && !isLoading) && (
-            < Chats
-              enableSidebar={false} chatId={data?.chatId}
-              emptyComponent={<EmptyState title="Bienvenido" icon="Update" description="Elige una sesión para ver las conversaciones" iconClassName="text-[#0DACC8]" />}
-            />
-          )
-        }
-      </Sessions>
-    </Workspace>
+    <MondayContex>
+      <Authorization>
+        <Workspace>
+          <Sessions type="small">
+            <SingleChat phoneColumnId={state.phoneColumnId} />
+          </Sessions>
+      </Workspace>
+      </Authorization>
+    </MondayContex>
+
   )
 }
 
