9712aea25c69bf4c05e61d3dee7f92e82f710272
DaniPoot <danielpoot.uc2@gmail.com>
2025-12-04 13:51:18 -0600

    🥅 Add new eror class for validations

--- src/errors/PublicError.ts
+++ src/errors/PublicError.ts
@@ -1,1 +1,12 @@
 export class PublicError extends Error {}
+
+export class ValidationError extends Error {
+  title: string
+  description: string
+
+  constructor(title: string, description: string) {
+    super(description)
+    this.title = title
+    this.description = description
+  }
+}
